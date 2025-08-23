65b89a4d37439350f02855184951da3e87697d50
Eliana <valdezeliana38@gmail.com>
2025-08-22 21:11:40 -0300

    Add initial views package with __init__.py and console.py

---
+++ views/console.py
@@ -0,0 +1,12 @@
+def print_menu():
+    print("""
+        \n------------------------------
+         \n\t\t MONGO CRUD CLI\n
+         \t - 1. Listar usuarios
+         \t - 2. Buscar un usuario por 'username'
+         \t - 3. Agregar un nuevo usuario
+         \t - 4. Actualizar un usuario por 'username'
+         \t - 5. Eliminar un usuario por 'username'
+         \t - 6. Salir\n
+          """)
+    
