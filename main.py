a31ca4f59c47e1eaeac2600b2be639947ad0d98b
Eliana <valdezeliana38@gmail.com>
2025-08-23 02:03:03 -0300

    Refactor: implement option_manager directly in main

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from views import console
 from controllers import user_crud
+from models.users import find_all_users
+
 if __name__ == "__main__":
     while True:
         console.print_menu()
-        user_crud.options_manager()
+        option = int(input("Ingrese el numero según la operación que desee realizar: "))
+        if option == 1:
+            find_all_users()
+        elif option == 2:
+            user_crud.search_by_username()
+        elif option == 3:
+            user_crud.insert_user()
+        elif option == 4:
+            user_crud.update_by_username()
+        elif option == 5:
+            user_crud.delete_by_username()
+        elif option == 6:
+            print(" ★ Gracias por usar Mongo Crud Cli")
+            break
