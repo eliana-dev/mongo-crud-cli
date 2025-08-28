118aca02c3fa1ef501a7a432eaa1df51ffbd2ee5
Eliana <valdezeliana38@gmail.com>
2025-08-28 01:23:31 -0300

    Refactor: replace hardcoded MongoDB configuration with environment variables

--- controllers/config.py
+++ controllers/config.py
@@ -1,5 +1,13 @@
-MONGO_HOST = "localhost"
-MONGO_PORT = 27017
-MONGO_USER = "pepito"
-MONGO_PASS = "perrito"
-MONGO_AUTH_MECHANISM = "SCRAM-SHA-256"
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+MONGO_HOST = os.getenv("MONGO_HOST")
+print(MONGO_HOST)
+MONGO_PORT = int(os.getenv("MONGO_PORT"))
+print(MONGO_PORT)
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_AUTH_MECHANISM = os.getenv("MONGO_AUTH_MECHANISM")
